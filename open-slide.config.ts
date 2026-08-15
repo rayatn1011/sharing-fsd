332d90b996ba61173c77612ddd4755ce7f3e6b88
ChunRen Zhang <80741494+rayatn1011@users.noreply.github.com>
2026-08-16 00:13:03 +0800

    ci: deploy OpenSlide to GitHub Pages

--- open-slide.config.ts
+++ open-slide.config.ts
@@ -1,5 +1,7 @@
 import type { OpenSlideConfig } from '@open-slide/core';
 
-const openSlideConfig: OpenSlideConfig = {};
+const openSlideConfig: OpenSlideConfig = {
+  base: '/sharing-fsd/',
+};
 
 export default openSlideConfig;
